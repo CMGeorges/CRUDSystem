fc16904f4e28e8b67984455e6f3ddc409080fea1
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2026-03-21 20:21:55 -0400

    Fix local build by removing missing DB file dependencies

--- CRUDSystem/Program.cs
+++ CRUDSystem/Program.cs
@@ -11,9 +11,9 @@ namespace CRUDSystem
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
         }
     }
 }
